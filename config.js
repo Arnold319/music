548f09fea415956dd6f11bcfefd2ebd4dbc493a5
Arnold319 <49011323+Arnold319@users.noreply.github.com>
2019-04-25 08:28:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.TOKEN = ('NTcwMzA1MjE2NjA0Mjc0Njk5.XMBk6w.G8whSJXQVDTIGaCyiCIFvC-L5CU');
 
 exports.PREFIX = 'm!';
 
-exports.GOOGLE_API_KEY = '2ea425be-f8d2-47c6-87bb-e99bd7d52471';
+exports.GOOGLE_API_KEY = 'AIzaSyBVihN481R4WFMgSXpL0scIC10d7y1i9NQ';
